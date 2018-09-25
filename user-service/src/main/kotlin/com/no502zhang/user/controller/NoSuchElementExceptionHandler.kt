0f52b7abeb7ad45e026fdefe9f7b75e1ca97bdfc
no502zhang <no502zhang@163.com>
2018-09-26 07:47:18 +0800

    由于404等很可能被代理或供应商等处理掉, 成功应全部返回200

--- user-service/src/main/kotlin/com/no502zhang/user/controller/NoSuchElementExceptionHandler.kt
+++ user-service/src/main/kotlin/com/no502zhang/user/controller/NoSuchElementExceptionHandler.kt
@@ -11,8 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class NoSuchElementExceptionHandler {
 
+    // 返回200状态, body中写异常信息
     @ExceptionHandler(NoSuchElementException::class)
     fun handleException(): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("NOT FOUND")
+        return ResponseEntity.ok().body("{\n" +
+                "    \"timestamp\": \"2018-09-25T23:25:17.373+0000\",\n" +
+                "    \"status\": 404,\n" +
+                "    \"error\": \"Not Found\",\n" +
+                "    \"message\": \"No message available\",\n" +
+                "    \"path\": \"/system/userss/41\"\n" +
+                "}")
     }
 }
