bc96f11133c1acca6f648fb4b9aee29890b3993e
no502zhang <no502zhang@163.com>
2018-09-28 08:06:09 +0800

    更新删除认证

--- security-service/src/main/kotlin/com/no502zhang/security/controller/TokenController.kt
+++ security-service/src/main/kotlin/com/no502zhang/security/controller/TokenController.kt
@@ -2,13 +2,25 @@ package com.no502zhang.security.controller
 
 import com.no502zhang.security.dto.CreateTokenParam
 import com.no502zhang.security.dto.CreateTokenResult
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.no502zhang.security.dto.UpdateTokenParam
+import com.no502zhang.security.dto.UpdateTokenResult
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("tokens")
 class TokenController {
-    fun createToken(param: CreateTokenParam): CreateTokenResult {
+    @PostMapping("")
+    fun createToken(@RequestBody param: CreateTokenParam): CreateTokenResult {
         return CreateTokenResult()
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteToken(@PathVariable("id") id: String) {
+
+    }
+
+    @PutMapping("/{id}")
+    fun updateToken(@PathVariable("id") id: String, @RequestBody param: UpdateTokenParam): UpdateTokenResult {
+        return UpdateTokenResult()
     }
 }
