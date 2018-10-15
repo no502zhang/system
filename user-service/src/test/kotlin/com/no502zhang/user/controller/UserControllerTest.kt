ff2d9a5ff5f06b0bc055da52f4ee1ba6c3284b09
no502zhang <no502zhang@163.com>
2018-10-15 18:46:16 +0800

    create user 测试

--- user-service/src/test/kotlin/com/no502zhang/user/controller/UserControllerTest.kt
+++ user-service/src/test/kotlin/com/no502zhang/user/controller/UserControllerTest.kt
@@ -1,5 +1,6 @@
 package com.no502zhang.user.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.no502zhang.security.client.AccountFeignClient
 import com.no502zhang.security.dto.CreateAccountParam
 import com.no502zhang.security.dto.CreateAccountResult
@@ -7,6 +8,7 @@ import com.no502zhang.user.UserTestStarterApplication
 import com.no502zhang.user.domain.User
 import com.no502zhang.user.dto.CreateUserParam
 import com.no502zhang.user.repository.UserRepository
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,9 +16,12 @@ import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import java.util.*
 
@@ -49,6 +54,11 @@ class UserControllerTest {
         Mockito.`when`(accountFeignClient.create(CreateAccountParam(user.id, param.account, param.password))).thenReturn(CreateAccountResult(UUID.randomUUID().toString().replace("-", ""), param.account))
 
 
-        println("test")
+        val request = MockMvcRequestBuilders.post("/users").contentType(MediaType.APPLICATION_JSON_UTF8).content(ObjectMapper().writeValueAsString(param))
+        val result = mvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk).andReturn()
+
+        assertEquals(result.response.status, 200)
+
+        println(result.response.contentAsString)
     }
 }
